2079d4f48a154dac60ded55b1651835b4288d156
Caner Al <caneral5858@gmail.com>
2022-05-10 20:23:39 +0300

    MaterialUI installed.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
 module.exports = {
-  mode: 'jit',
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  mode: "jit",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  important: "#root",
   theme: {
     extend: {
       boxShadow: {
-        'all' : '0px 4px 24px 0 rgba(34, 41, 47, 0.1)'
-      }
+        all: "0px 4px 24px 0 rgba(34, 41, 47, 0.1)",
+      },
     },
   },
   plugins: [],
-}
+};
